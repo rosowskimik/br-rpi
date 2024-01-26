37086d02ab02b2421dbe6cda70bc65422286e07e
Mikołaj Rosowski <m.rosowski1@wp.pl>
2024-01-26 22:24:36 +0100

    br(package): update card_reader package
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- package/card_reader/card_reader.mk
+++ package/card_reader/card_reader.mk
@@ -3,17 +3,24 @@
 # Card reader
 #
 ################################################################################
-CARD_READER_VERSION = fa7d1a66cc8cb6e70ba0339286fafcb6860e8ebe
+CARD_READER_VERSION = ba874f1ffdeecb907f26d74d1bdbea5272b94142
 CARD_READER_SITE = $(call github,rosowskimik,card_reader,$(CARD_READER_VERSION))
 CARD_READER_LICENSE = MIT
 CARD_READER_LICENSE_FILES = LICENSE
 
 
 define CARD_READER_INSTALL_CONFIG
-	$(INSTALL) -D -m 644 $(CARD_READER_PKGDIR)/default_config.yml \
+	$(INSTALL) -D -m 644 $(CARD_READER_PKGDIR)/config_template.yml \
 		$(TARGET_DIR)/etc/xdg/card_reader/config.yml
 
-	$(SED) 's/localhost/$(call qstrip,$(BR2_PACKAGE_CARD_READER_HOSTNAME))/' \
+	$(SED) 's#@INIT_LOCKED@#$(if $(BR2_PACKAGE_CARD_READER_INIT_LOCKED),true,false)#' \
+		-e 's#@INTERFACE@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_INTERFACE))#' \
+		-e 's#@HOSTNAME@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_HOSTNAME))#' \
+		-e 's#@RED_LED@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_RED_LED))#' \
+		-e 's#@GREEN_LED@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_GREEN_LED))#' \
+		-e 's#@STRENGTH@#$(BR2_PACKAGE_CARD_READER_STRENGTH)#' \
+		-e 's#@CARD_TIMEOUT@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_CARD_TIMEOUT))#' \
+		-e 's#@MOVEMENT_TIMEOUT@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_MOVEMENT_TIMEOUT))#' \
 		$(TARGET_DIR)/etc/xdg/card_reader/config.yml
 endef
 CARD_READER_POST_INSTALL_TARGET_HOOKS += CARD_READER_INSTALL_CONFIG
