ab81a9738714b7d0dd53690232a1e082a82d1907
Mikołaj Rosowski <m.rosowski1@wp.pl>
2024-01-28 19:56:11 +0100

    card_reader: update API config
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- package/card_reader/card_reader.mk
+++ package/card_reader/card_reader.mk
@@ -3,7 +3,7 @@
 # Card reader
 #
 ################################################################################
-CARD_READER_VERSION = ba874f1ffdeecb907f26d74d1bdbea5272b94142
+CARD_READER_VERSION = 1f08474d492b13b47e76b0c30cbc618188ca2f9f
 CARD_READER_SITE = $(call github,rosowskimik,card_reader,$(CARD_READER_VERSION))
 CARD_READER_LICENSE = MIT
 CARD_READER_LICENSE_FILES = LICENSE
@@ -14,6 +14,7 @@ define CARD_READER_INSTALL_CONFIG
 		$(TARGET_DIR)/etc/xdg/card_reader/config.yml
 
 	$(SED) 's#@INIT_LOCKED@#$(if $(BR2_PACKAGE_CARD_READER_INIT_LOCKED),true,false)#' \
+		-e 's#@SYSTEM_ID@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_SYSTEM_ID))#' \
 		-e 's#@INTERFACE@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_INTERFACE))#' \
 		-e 's#@HOSTNAME@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_HOSTNAME))#' \
 		-e 's#@RED_LED@#$(call qstrip,$(BR2_PACKAGE_CARD_READER_RED_LED))#' \
